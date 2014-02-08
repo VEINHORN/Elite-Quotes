e135541080a36756ee16ce77ff8efcfa6f39efd5
veinhorn <mcklas@mail.ru>
2014-02-08 23:19:11 +0300

    Some fixes.

--- app/src/main/java/com/elitequotes/MainActivity.java
+++ app/src/main/java/com/elitequotes/MainActivity.java
@@ -2,13 +2,30 @@ package com.elitequotes;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
-
+    private TextView quoteTextView;
+    private TextView quoteAuthorTextView;
+    private Button reloadButton;
+    private QuoteLoader quoteLoader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        quoteTextView = (TextView)findViewById(R.id.quoteTextView);
+        quoteAuthorTextView = (TextView)findViewById(R.id.quoteAuthorTextView);
+        reloadButton = (Button)findViewById(R.id.reloadButton);
+        reloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quoteLoader = new QuoteLoader(quoteTextView, quoteAuthorTextView);
+                quoteLoader.execute();
+            }
+        });
+        quoteLoader = new QuoteLoader(quoteTextView, quoteAuthorTextView);
+        quoteLoader.execute();
     }
 }
