3278f9730b74454a11550c2e56bb6218f0477a5c
VEINHORN <mcklas@mail.ru>
2014-03-20 00:13:54 +0300

    Added spinner to ActionBar.

--- app/src/main/java/com/elitequotes/MainActivity.java
+++ app/src/main/java/com/elitequotes/MainActivity.java
@@ -3,15 +3,18 @@ package com.elitequotes;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.text.Html;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.ads.AdRequest;
 import com.google.ads.AdView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
     private TextView quoteTextView;
@@ -24,12 +27,29 @@ public class MainActivity extends ActionBarActivity {
     private RelativeLayout mainLayout;
     private BackgroundLoader backgroundLoader;
 
+    private ArrayList<SpinnerNavigationItem> spinnerNavigationItems;
+    private ArrayList<String> titles;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0A2229")));
-        getSupportActionBar().setTitle(Html.fromHtml("<b><font color=\"#BDBDBD\">" + getString(R.string.app_name) + "</font></b>"));
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0A2229")));
+        actionBar.setDisplayShowTitleEnabled(false);
+        //actionBar.setTitle(Html.fromHtml("<b><font color=\"#BDBDBD\">" + getString(R.string.app_name) + "</font></b>"));
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        spinnerNavigationItems = new ArrayList<SpinnerNavigationItem>();
+        titles = new ArrayList<String>();
+        titles.add("Elite Quotes");
+        titles.add("Favourite");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.spinner_list_item, R.id.spinner_item_title, titles);
+        actionBar.setListNavigationCallbacks(arrayAdapter, new ActionBar.OnNavigationListener() {
+            @Override
+            public boolean onNavigationItemSelected(int i, long l) {
+                return false;
+            }
+        });
         quoteTextView = (TextView)findViewById(R.id.quoteTextView);
         quoteAuthorTextView = (TextView)findViewById(R.id.quoteAuthorTextView);
         reloadButton = (ImageButton)findViewById(R.id.reloadButton);
@@ -37,10 +57,8 @@ public class MainActivity extends ActionBarActivity {
         facebookButton = (ImageButton)findViewById(R.id.facebookButton);
         vkButton = (ImageButton)findViewById(R.id.vkButton);
         mainLayout = (RelativeLayout)findViewById(R.id.main_layout);
-
         backgroundLoader = new BackgroundLoader(this, mainLayout);
         backgroundLoader.selectBackground();
-
         reloadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,10 +87,8 @@ public class MainActivity extends ActionBarActivity {
                 shareUtil.shareOnVk();
             }
         });
-
         AdView adView = (AdView)findViewById(R.id.activity_main_adView);
         adView.loadAd(new AdRequest());
-
         quoteLoader = new QuoteLoader(MainActivity.this, quoteTextView, quoteAuthorTextView);
         quoteLoader.execute();
     }
---
+++ app/src/main/java/com/elitequotes/SpinnerNavigationItem.java
@@ -0,0 +1,16 @@
+package com.elitequotes;
+
+/**
+ * Created by veinhorn on 19.3.14.
+ */
+public class SpinnerNavigationItem {
+    private String title;
+
+    public SpinnerNavigationItem(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
