aef077a750a46737fb171e88faef40f5f0498dd6
VEINHORN <mcklas@mail.ru>
2014-04-03 22:26:49 +0300

    Added backgrounds to Favourite.

--- app/src/main/java/com/elitequotes/favourite/FavouriteFragment.java
+++ app/src/main/java/com/elitequotes/favourite/FavouriteFragment.java
@@ -6,10 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.elitequotes.R;
 import com.elitequotes.database.DatabaseHandler;
+import com.elitequotes.loaders.BackgroundLoader;
 
 /**
  * Created by veinhorn on 20.3.14.
@@ -19,11 +21,16 @@ public class FavouriteFragment extends Fragment {
     private FavouriteElementsAdapter favouriteElementsAdapter;
     private FavouriteElementsContainer favouriteElementsContainer;
     private DatabaseHandler databaseHandler;
+    private LinearLayout linearLayout;
+    private BackgroundLoader backgroundLoader;
 
     @Override
     public View onCreateView(LayoutInflater layoutInflater, ViewGroup container, Bundle savedInstanceState) {
         Activity activity = getActivity();
         View rootView = layoutInflater.inflate(R.layout.favourite_fragment, container, false);
+        linearLayout = (LinearLayout)rootView.findViewById(R.id.favourite_layout);
+        backgroundLoader = new BackgroundLoader(activity, linearLayout);
+        backgroundLoader.selectBackground();
         favouriteListView = (ListView)rootView.findViewById(R.id.favourite_listview);
         favouriteElementsContainer = new FavouriteElementsContainer();
         databaseHandler = new DatabaseHandler(activity);
--- app/src/main/java/com/elitequotes/loaders/BackgroundLoader.java
+++ app/src/main/java/com/elitequotes/loaders/BackgroundLoader.java
@@ -1,7 +1,7 @@
 package com.elitequotes.loaders;
 
 import android.content.Context;
-import android.widget.RelativeLayout;
+import android.view.ViewGroup;
 
 import com.elitequotes.R;
 
@@ -12,12 +12,12 @@ import java.util.Random;
  */
 public class BackgroundLoader {
     private Context context;
-    private RelativeLayout relativeLayout;
+    private ViewGroup relativeLayout;
     private int[] backgrounds = { R.drawable.background, R.drawable.background2, R.drawable.background3, R.drawable.background4, R.drawable.background5, R.drawable.background6 };
     private final static int MIN = 0;
     private final static int MAX = 5;
 
-    public BackgroundLoader(Context context, RelativeLayout relativeLayout) {
+    public BackgroundLoader(Context context, ViewGroup relativeLayout) {
         this.context = context;
         this.relativeLayout = relativeLayout;
     }
